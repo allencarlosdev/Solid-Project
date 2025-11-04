03c713d458954708e089c4fb778d6196d2d56260
allencarlosdev <allencarlosdev@gmail.com>
2025-11-04 17:07:58 -0400

    feat: TDD - Index Book

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+ // CRUD - Book
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
